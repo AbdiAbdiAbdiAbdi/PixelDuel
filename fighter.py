92ef9811872f08868e28ce1c9e216eeeb963d618
Suhaib Hussein <husseinsuhaib5@gmail.com>
2025-07-18 18:29:09 -0500

    Updated background and removed Blob Battles title

--- fighter.py
+++ fighter.py
@@ -1,93 +1,77 @@
 import pygame
 
-class Fighter():
-    #init func
-    def __init__(self,x,y):
-        self.flip = False
-        self.rect = pygame.Rect((x, y, 80, 180))
+class Fighter:
+    def __init__(self, x, y, color, controls):
+        self.rect = pygame.Rect(x, y, 80, 80)  # Blob size (80x80)
+        self.color = color
+        self.controls = controls
         self.vel_y = 0
         self.jump = False
-        self.num_of_jumps = 0
-        self.attack_type = 0
-        self.attacking = False
         self.health = 100
-    
-    def move(self, screen_width, screen_height, surface, target):
-        SPEED = 10
-        GRAVITY = 2
-        # d = delta (change in axis)
+        self.attacking = False
+        self.speed = 8
+        self.gravity = 2
+
+    def move(self, screen_width, screen_height, surface, target, effects):
         dx = 0
         dy = 0
 
+        keys = pygame.key.get_pressed()
 
-        #get keypress
-        key = pygame.key.get_pressed()
+        if not self.attacking:
+            if keys[self.controls['left']]:
+                dx = -self.speed
+            if keys[self.controls['right']]:
+                dx = self.speed
 
-        #can only do the following if not attacing
-        if self.attacking == False:
-            #movement keys
-            if key[pygame.K_a]:
-                dx = -SPEED
-            if key[pygame.K_d]:
-                dx = SPEED
-            
-            #jump
-            #TODO add Double Jump
-            if key[pygame.K_k] and self.jump == False:
-                self.vel_y = -40
+            if keys[self.controls['jump']] and not self.jump:
+                self.vel_y = -30
                 self.jump = True
-            
-            #attack key
-            if key[pygame.K_j]:
-                self.attack_type = 1
-                self.attack(surface, target)
-            
 
+            if keys[self.controls['attack_punch']]:
+                self.attack(surface, target, effects, "punch")
+            elif keys[self.controls['attack_special']]:
+                self.attack(surface, target, effects, "special")
 
-        #applyin gravity
-        self.vel_y += GRAVITY
+        self.vel_y += self.gravity
         dy += self.vel_y
 
-        #make sure play stays on screen
+        # Stay on screen horizontally
         if self.rect.left + dx < 0:
             dx = -self.rect.left
-
-        if self.rect.right + dx > 1000:
+        if self.rect.right + dx > screen_width:
             dx = screen_width - self.rect.right
-        
-        if self.rect.bottom + dy > screen_height - 21:
+
+        # Stay on floor vertically
+        floor = screen_height - 50
+        if self.rect.bottom + dy > floor:
+            dy = floor - self.rect.bottom
             self.vel_y = 0
             self.jump = False
-            dy = screen_height - 21 - self.rect.bottom
 
-        #make sure players face each other
-        if target.rect.centerx > self.rect.centerx:
-            self.flip = False
-
-        else:
-            self.flip = True
-
-        #update player postion
         self.rect.x += dx
         self.rect.y += dy
 
-    
-    #attack func
-    def attack(self, surface, target):
+    def attack(self, surface, target, effects, attack_type):
         self.attacking = True
-        attacking_rect = pygame.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height)
+        attack_rect = pygame.Rect(self.rect.centerx - 40, self.rect.y, 80, 80)
+        if attack_rect.colliderect(target.rect):
+            target.health -= 5 if attack_type == "punch" else 10
+            # Add hit effect, sounds etc here
+        # Reset attacking after small delay (simple example)
+        pygame.time.set_timer(pygame.USEREVENT + 1, 300)
 
-        if attacking_rect.colliderect(target.rect):
-            print("Hit!")
-            target.health -= 5
-
-        
-
-
-
-        pygame.draw.rect(surface, (0, 255, 0), attacking_rect)
-
-    #draw func
     def draw(self, surface):
-        pygame.draw.rect(surface, (255, 0, 0), self.rect)
+        center = self.rect.center
+        radius = self.rect.width // 2
 
+        pygame.draw.circle(surface, self.color, center, radius)
+
+        # Subtle highlight for 3D effect
+        highlight_color = (
+            min(self.color[0] + 60, 255),
+            min(self.color[1] + 60, 255),
+            min(self.color[2] + 60, 255),
+        )
+        highlight_pos = (center[0] - radius // 3, center[1] - radius // 3)
+        pygame.draw.circle(surface, highlight_color, highlight_pos, radius // 2)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from fighter import Fighter
+# from effects import HitEffect  # Uncomment if you have this
 
 pygame.init()
 
@@ -7,74 +8,106 @@ SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Brawler")
+pygame.display.set_caption("Blob Battles")
 
-#load background image
-bg_img = pygame.image.load("assets/background/backdrop.jpg").convert_alpha()
-#scale background to screen
-scale_bg = pygame.transform.scale(bg_img, (SCREEN_WIDTH, SCREEN_HEIGHT))
-#Set up clock
+# Load background image
+try:
+    bg_img = pygame.image.load("assets/background.jpg").convert_alpha()
+    scale_bg = pygame.transform.scale(bg_img, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Background loaded successfully!")
+except Exception as e:
+    print(f"Error loading background image: {e}")
+    scale_bg = None
+
 clock = pygame.time.Clock()
 
-#define colors (some might not be used)
 YELLOW = (255, 255, 0)
-RED = (255, 0, 0)
-WHITE = (255, 255, 255)
 GREY = (128, 128, 128)
+WHITE = (255, 255, 255)
 
+font = pygame.font.SysFont('arialblack', 24)
 
-#draw background image func
+effects = []  # Placeholder for effects list
+
 def draw_bg():
-    screen.blit(scale_bg, (0, 0))
+    if scale_bg:
+        screen.blit(scale_bg, (0, 0))
+    else:
+        screen.fill((30, 30, 40))
 
-#func to draw health bars
 def draw_health_bar(health, x, y):
-    ratio_of_vitality = health / 100
-    pygame.draw.rect(screen, GREY, (x-5, y+2, 410, 35))
-    pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio_of_vitality, 30))
+    ratio = health / 100
+    pygame.draw.rect(screen, GREY, (x - 5, y + 2, 410, 35))
+    pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
 
+def draw_attack_text(text, x, y):
+    render = font.render(text, True, WHITE)
+    screen.blit(render, (x, y))
 
-#instatiate two fighters
-fighter1 = Fighter(200, 400)
-fighter2 = Fighter(700, 400)
+# Controls for red blob
+controls1 = {
+    'left': pygame.K_a,
+    'right': pygame.K_d,
+    'jump': pygame.K_w,
+    'attack_punch': pygame.K_f,
+    'attack_special': pygame.K_g
+}
 
+# Controls for blue blob
+controls2 = {
+    'left': pygame.K_LEFT,
+    'right': pygame.K_RIGHT,
+    'jump': pygame.K_UP,
+    'attack_punch': pygame.K_SLASH,
+    'attack_special': pygame.K_PERIOD
+}
+
+fighter1 = Fighter(200, 400, (255, 0, 0), controls1)
+fighter2 = Fighter(700, 400, (0, 0, 255), controls2)
 
 run = True
-
 while run:
-    #Caps framerate at 60FPS
     clock.tick(60)
+    attack1_text = ""
+    attack2_text = ""
 
-    
-
-    #event handeler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run  = False
+            run = False
+
+    keys = pygame.key.get_pressed()
+
+    # Attack text for red blob
+    if keys[controls1['attack_punch']]:
+        attack1_text = "Red: Punch!"
+    elif keys[controls1['attack_special']]:
+        attack1_text = "Red: Special Attack!"
+
+    # Attack text for blue blob
+    if keys[controls2['attack_punch']]:
+        attack2_text = "Blue: Punch!"
+    elif keys[controls2['attack_special']]:
+        attack2_text = "Blue: Special Attack!"
 
     draw_bg()
+    # Title removed here, no draw_title()
 
-    #show players health
     draw_health_bar(fighter1.health, 20, 20)
     draw_health_bar(fighter2.health, 580, 20)
 
+    fighter1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter2, effects)
+    fighter2.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter1, effects)
 
-    #move fighters
-    fighter1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter2)
-    #fighter2.move(SCREEN_WIDTH)
-
-    #draw fighters
     fighter1.draw(screen)
     fighter2.draw(screen)
 
+    # Effects update/draw code if any
 
-    #updates screen
+    if attack1_text:
+        draw_attack_text(attack1_text, 20, 70)
+    if attack2_text:
+        draw_attack_text(attack2_text, 580, 70)
+
     pygame.display.update()
-    
-
-
-
-#exit pygame
 
 pygame.quit()
-
